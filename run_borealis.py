43e9c548b0ebcf4216e936a33e797f335143d2d9
skull132 <erki.meinberg@gmail.com>
2017-04-08 14:04:51 +0300

    Better auto-restarting with batch Since the python version really did not work.

--- run_borealis.py
+++ run_borealis.py
@@ -1,32 +1,18 @@
 from borealis import BotBorealis
 import time
 
-while True:
-	bot = None
+try:
+	print("Initializing BOREALIS and its subcomponents.")
+	bot = BotBorealis("config.yml")
 
-	try:
-		print("Welcome to BOREALIS.")
-		print("Initializing BOREALIS and its subcomponents.")
-		bot = BotBorealis("config.yml")
+	print("Initialization completed. Readying subcomponents.")
+	bot.setup()
 
-		print("Initialization completed. Readying subcomponents.")
-		bot.setup()
+	print("Subcomponents ready. All systems functional.")
+	print("Starting BOREALIS.")
+	bot.start_borealis()
+except Exception as e:
+	print("Danger! Exception caught!")
+	print(e)
 
-		print("Subcomponents ready. All systems functional.")
-		print("Starting BOREALIS.")
-		bot.start_borealis()
-	except Exception as e:
-		print("Danger! Exception caught!")
-		print(e)
-
-	print("Deleting bot!")
-	# Delete the bot, run it again.
-	del bot
-
-	# Sleep for a bit before restarting!
-	time.sleep(60)
-	print("Restarting loop.\n\n\n")
-
-# Should never get here, but just in case.
-print("We somehow exited the main loop. :ree:")
-input("Press Enter to exit.")
+print("Bot exited.")
