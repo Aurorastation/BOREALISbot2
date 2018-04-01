73b8ebb178a499527a2e2dba88ff2f9ea5beeab2
skull132 <erki.meinberg@gmail.com>
2018-04-01 20:15:45 +0300

    Changelog command You can now access the recent changes made to the bot! Hurray, you get to read my shitty commit messages!

--- cogs/git.py
+++ cogs/git.py
@@ -1,6 +1,10 @@
 import os
+from datetime import datetime
+
 from git import Repo
 from discord.ext import commands
+
+from .utils.paginator import FieldPages
 
 class GitCog:
     def __init__(self, bot):
@@ -24,6 +28,34 @@ class GitCog:
             remote.pull()
         except Exception as err:
             await ctx.send(f"Error while updating:\n{err}")
+
+    @commands.command()
+    async def changelog(self, ctx, count: int=5):
+        """Displays the changelog!"""
+        if count > 20 or count < 1:
+            await ctx.send("Sorry, I can only show 1 ... 20 entries for you!")
+            return
+
+        repo = Repo(os.getcwd())
+
+        if not repo or repo.bare:
+            await ctx.send("Repository is not properly set up. No log to show.")
+            return
+
+        commits = repo.iter_commits('master', max_count=count)
+
+        if not commits:
+            await ctx.send("No commits found.")
+            return
+
+        entries = []
+        for commit in commits:
+            time = datetime.fromtimestamp(commit.committed_date).strftime("%Y-%m-%d")
+            entries.append((f"{time}-{commit.binsha}", f"{commit.message}"))
+
+        p = FieldPages(ctx, entries=entries, per_page=5)
+        p.embed.title = "Latest changes"
+        await p.paginate()
 
 def setup(bot):
     bot.add_cog(GitCog(bot))
