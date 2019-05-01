3dd9c895477f5003641d47fa2067533cfadc9ba8
Mykhailo Bykhovtsev <mikebykhovtsev@gmail.com>
2019-05-01 07:08:12 -0700

    Renames the server-status admins field to "staff" to avoid confusion (#28)
    When you request status from bot, it counts anyone into Admins field. Well this PR renames it to Staff to avoid confusion.

--- cogs/server.py
+++ cogs/server.py
@@ -83,6 +83,7 @@ class ServerCog():
         embed.add_field(name="Duration:", value=data["roundduration"])
         embed.add_field(name="Mode:", value=data["mode"])
         embed.add_field(name="Players:", value=data["players"])
+        embed.add_field(name="Staff:", value=data["staff"])
         embed.add_field(name="Admins:", value=data["admins"])
         await ctx.send(embed=embed)
 
